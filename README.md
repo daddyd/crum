799a852af642391fb759c871f9e9aa1cc810605e
Dirk Dierickx <dirk.dierickx@gmail.com>
2019-12-17 21:51:33 +0100

    initial release v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # crum
 Catalog Removable USB Media
+
+Do you have a large collection of removable USB media containing massive amounts of files?
+Do you have difficulty finding back those files on your media?
+
+CRUM helps you in cataloging your USB media and locating the file you're looking for.
---
+++ crum.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+# CRUM - Catalog Removable USB Media
+
+VERSION='0.1.0'
+
+CRUMDBDIR="${HOME}/.crum"
+CRUMDB="${CRUMDBDIR}/crumdb"
+
+check_crumdbdir() {
+        if [[ ! -d "${CRUMDBDIR}" ]]; then
+                mkdir "${CRUMDBDIR}" || return 1
+        fi
+        return 0
+}
+
+helpmsg() {
+        echo "Usage:"
+        echo " crum.sh -v|h"
+        echo " crum.sh -a|r <mountpoint>"
+        echo " crum.sh -f <file>"
+        echo ""
+        echo "Options:"
+        echo " -a       add usb media to catalog"
+        echo " -r       remove usb media from catalog"
+        echo " -f       find specified file"
+        echo ""
+        echo " -h       display this help"
+        echo " -v       display version"
+}
+
+add_media() {
+        MOUNTPOINT="${1}"
+        if [[ -d "${MOUNTPOINT}" ]]; then
+                MEDIANAME=$(basename "${MOUNTPOINT}")
+                find "${MOUNTPOINT}" |
+                while IFS= read -r FOUND;
+                do
+                        echo "${MEDIANAME},${FOUND}" >> "${CRUMDB}"
+                done
+        fi
+}
+
+remove_media() {
+        MEDIANAME="${1}"
+        sed -i /"${MEDIANAME}"/d "${CRUMDB}"
+}
+
+find_file() {
+        FILE="${1}"
+        grep "${FILE}" "${CRUMDB}" | cut -d, -f1
+}
+
+check_crumdbdir || exit 1
+while getopts 'hva:r:' OPT
+do
+        case "${OPT}" in
+                'v') echo "crum.sh v${VERSION}"
+                     ;;
+                'h') helpmsg
+                     ;;
+                'a') add_media "${OPTARG}"
+                     ;;
+                'f') find_file "${OPTARG}"
+                     ;;
+                'r') remove_media "${OPTARG}"
+                     ;;
+                *)   echo "Invalid option: ${OPT}"
+                     helpmsg
+                     ;;
+        esac
+done
+
+exit 0;
+                   
